bc3d80389034e8eed2e59bf41fc8e09b2c87cc08
Vadim G <rrretry@ya.ru>
2020-04-21 04:45:05 +0500

    create match on human creation

--- match/models.py
+++ match/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from faker import Faker
 
 from users.models import Human
 
@@ -17,3 +19,19 @@ class Match(models.Model):
     age = models.SmallIntegerField()
     gender = models.IntegerField(choices=GENDER_CHOICES)
 
+
+def create_match(sender, instance, **kwargs):
+    faker = Faker()
+    gender_suffix = Human.GENDER_CHOICES[not instance.gender][1].lower()
+    name = getattr(faker, f"name_{gender_suffix}")()
+    name, second_name = name.split()[:2]
+    Match(
+        human_id=instance,
+        first_name=name,
+        second_name=second_name,
+        gender=instance.gender,
+        age=instance.age + faker.random_digit()
+    ).save()
+
+
+post_save.connect(create_match, sender=Human)
