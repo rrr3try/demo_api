cde713744e38d1ec4a792530f17bc098c57ed928
Vadim G <rrretry@ya.ru>
2020-04-21 04:43:13 +0500

    add match model

--- match/models.py
+++ match/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Human
+
+
+class Match(models.Model):
+    human_id = models.ForeignKey(Human, on_delete=models.CASCADE)
+    MALE = 0
+    FEMALE = 1
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    ]
+
+    first_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128)
+    age = models.SmallIntegerField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from match.models import Match
 from users.models import Human
 
 admin.site.register(Human)
+admin.site.register(Match)
