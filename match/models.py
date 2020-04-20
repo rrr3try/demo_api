c853fc0795e1b20998bf36e106f34c14e099543f
Vadim G <rrretry@ya.ru>
2020-04-21 04:45:44 +0500

    add match migrations, cut unnecessary code

--- match/models.py
+++ match/models.py
@@ -7,17 +7,10 @@ from users.models import Human
 
 class Match(models.Model):
     human_id = models.ForeignKey(Human, on_delete=models.CASCADE)
-    MALE = 0
-    FEMALE = 1
-    GENDER_CHOICES = [
-        (MALE, 'Male'),
-        (FEMALE, 'Female')
-    ]
-
     first_name = models.CharField(max_length=128)
     second_name = models.CharField(max_length=128)
     age = models.SmallIntegerField()
-    gender = models.IntegerField(choices=GENDER_CHOICES)
+    gender = models.IntegerField(choices=Human.GENDER_CHOICES)
 
 
 def create_match(sender, instance, **kwargs):
