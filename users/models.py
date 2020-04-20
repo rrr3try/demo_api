a1c5a3ea0d80e240ed51200f427b5120364856fc
Vadim G <rrretry@ya.ru>
2020-04-20 21:25:58 +0500

    make Human model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Human(models.Model):
+    MALE = 0
+    FEMALE = 1
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    ]
+
+    avatar = models.FileField()
+    first_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128)
+    age = models.SmallIntegerField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)
